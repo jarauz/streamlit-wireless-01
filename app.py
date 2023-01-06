bd8206b75fbf5e307e74c6523165c62fe437cc03
Julio Arauz <jarauz@alumni.pitt.edu>
2023-01-06 15:30:42 +0000

    Added fsp coded

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.image("ohio_logo_small.svg", width=200)
 st.caption("McClure School of Emerging Communicaton Technologies")
 
 
-tab1, tab2, tab3, tab4 = st.tabs(["Power", "Path loss", "Free space propagation", "Fox"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Power", "Path loss", "Free space propagation", "SNR", "Shannon"])
 
 with tab1:
   col1, col2 = st.columns(2)
@@ -83,13 +83,75 @@ with tab3:
   col1, col2 = st.columns(2)
   with col1:
     st.subheader('Free space propagation')
-    n6 = st.number_input('Power transmitted in [dBm]', key='n6')
-    n7 = st.number_input('Power transmitted in [dBm]', key='n7')
-    n8 = st.number_input('Path Loss in [dB]', key='n8')
-    n9 = st.number_input('Gain transmitting antenna [dB]', key='n9')
-    n10 = st.number_input('Gain receiving antenna [dB]', key='n10')
-    st.latex(r'''Prx_{[dBm]}=Ptx_{[dBm]}-PL_{[dB]}+Gtx_{[dB]}+Grx_{[dB]}''')   
+    st.caption('Type values of known parameters in the boxes.')
+    n6 = st.number_input('Power received in [dBm] (Prx)', key='n6')
+    n7 = st.number_input('Power transmitted in [dBm] (Ptx)', key='n7')
+    n8 = st.number_input('Path Loss in [dB] (PL)', key='n8')
+    n9 = st.number_input('Gain transmitting antenna [dB] (Gtx)', key='n9')
+    n10 = st.number_input('Gain receiving antenna [dB] (Grx)', key='n10')
+    st.latex(r'''Prx_{[dBm]}=Ptx_{[dBm]}-PL_{[dB]}+Gtx_{[dB]}+Grx_{[dB]}''')
+
+    result4 = st.button(label="Compute Prx in [dBm]")
+    result5 = st.button(label="Compute Ptx in [dBm")
+    result6 = st.button(label="Compute PL in [dB]")
+    result7 = st.button(label="Compute Gtx in [dB]")
+    result8 = st.button(label="Compute Grx in [dB]")
+    
+  with col2:
+    st.subheader('Model results')
+    st.caption('Use the buttons to compute the unknown parameter and display the results.')
+    if result4:
+      prx = n7-n8+n9+n10
+      output = "{:.4f}".format(prx)
+      st.write('Power transmitted in [dBm] is ', n7, '[dBm]')
+      st.write('Path loss in [dB] is ', n8, '[dB]')
+      st.write('Gain transmitting antenna in [dB] is', n9, '[dB]')
+      st.write('Gain of receiving antenna in [db] is ', n10, '[dB]')
+      st.write('Power received in [dBm] is ', output, '[dBm]')
+
+    if result5:
+      ptx = n6+n8-n9-n10
+      output = "{:.4f}".format(ptx)
+      st.write('Power received in [dBm] is ', n6, '[dBm]')
+      st.write('Path loss in [dB] is ', n8, '[dB]')
+      st.write('Gain transmitting antenna in [dB] is', n9, '[dB]')
+      st.write('Gain of receiving antenna in [db] is ', n10, '[dB]')
+      st.write('Power transmitted in [dBm] is ', output, '[dBm]')
+      st.latex(r'''Ptx_{[dBm]}=Prx_{[dBm]}+PL_{[dB]}-Gtx_{[dB]}-Grx_{[dB]}''')
+
+    if result6:
+      pl = n7-n6+n9+n10
+      output = "{:.4f}".format(pl)
+      st.write('Power received in [dBm] is ', n6, '[dBm]')
+      st.write('Power transmitted in [dBm] is ', n7, '[dBm]')
+      st.write('Gain transmitting antenna in [dB] is', n9, '[dB]')
+      st.write('Gain of receiving antenna in [db] is ', n10, '[dB]')
+      st.write('Path loss in [dB] is ', output, '[dB]')
+      st.latex(r'''PL_{[dB]}=Ptx_{[dBm]}-Prx_{[dBm]}+Gtx_{[dB]}+Grx_{[dB]}''')
+
+    if result7:
+      gtx = n6-n7+n8-n10
+      output = "{:.4f}".format(grx)
+      st.write('Power received in [dBm] is ', n6, '[dBm]')
+      st.write('Power transmitted in [dBm] is ', n7, '[dBm]')
+      st.write('Path loss in [dB] is ', n8, '[dB]')
+      st.write('Gain of receiving antenna in [db] is ', n10, '[dB]')
+      st.write('Gain transmitting antenna in [dB] is', output, '[dB]')
+      st.latex(r'''Gtx_{[dB]}=Ptx_{[dBm]}-Prx_{[dBm]}+PL_{[dB]}-Grx_{[dB]}''')
+
+    if result8:
+      grx = n6-n7+n8-n9
+      output = "{:.4f}".format(grx)
+      st.write('Power received in [dBm] is ', n6, '[dBm]')
+      st.write('Power transmitted in [dBm] is ', n7, '[dBm]')
+      st.write('Path loss in [dB] is ', n8, '[dB]')
+      st.write('Gain transmitting antenna in [dB] is', n9, '[dB]')
+      st.write('Gain of receiving antenna in [db] is ', output, '[dB]')
+      st.latex(r'''Grx_{[dB]}=Ptx_{[dBm]}-Prx_{[dBm]}+PL_{[dB]}-Gtx_{[dB]}''')
 
 with tab4:
-  st.header("An owl")
-  st.image("https://static.streamlit.io/examples/owl.jpg", width=290)
+  st.subheader("Signal to noise computations")
+
+with tab5:
+  st.subheader("Shannon-Hartley channel bandwidth")
+
