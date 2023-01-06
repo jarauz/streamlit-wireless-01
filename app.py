e70ecd36c3d0297079b2256ed7f9cd410ad9ab34
Julio Arauz <jarauz@alumni.pitt.edu>
2023-01-06 18:04:14 +0000

    Wide mode added

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ import streamlit as st
 import numpy as np
 import time
 
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 resultComputeOne = 1
 
 def computeOne(a):
